64da6b9723d7215546db8c40f2e3dd0d3aa8c3c9
psotos <peter.sotos@tcs.com>
2023-09-08 14:40:44 -0400

    Fixed bug in Vehicles Repository

--- src/main/java/com/tcs/natd/springhandson/parkinggarage/repository/VehicleRepository.java
+++ src/main/java/com/tcs/natd/springhandson/parkinggarage/repository/VehicleRepository.java
@@ -14,7 +14,7 @@ public interface VehicleRepository  {
     Vehicle findByLicensePlate(String licensePlate);
     List<Vehicle> findAllByUserId(Long userId);
 
-    @Query(value="SELECT vehicles.id, vehicles.make_id, vehicles.license_plate, vehicles.user_id FROM "
+    @Query(value="SELECT vehicles.id, vehicles.make_id, vehicles.license_plate, vehicles.user_id, vehicle.color FROM "
             + "vehicles JOIN makes on vehicles.make_id = makes.id where makes.name = ?", nativeQuery = true)
     List<Vehicle> findAllByMakeName(String make);
 }
